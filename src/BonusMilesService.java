2747695e40a2f81cfc34dee9a01a2ed120c4dfdd
Liubov Neto <j.lubs3@gmail.com>
2025-12-07 19:12:20 +0300

    hw03_01

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        // 1 миля за каждые 20 рублей
+        int miles = price / 20;
+        return miles;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        {   int price = 10_000;
+            int miles = service.calculate(price); // должно получиться 500
+            System.out.println(miles);
+        }
+        {
+            int price = 5_000; // 250 миль
+            int miles = service.calculate(price); // должно получиться 500
+            System.out.println(miles);
+        }
+
+        {
+            int price = 19_999; // 999 миль (19_999 / 20 = 999)
+            int miles = service.calculate(price); // должно получиться 500
+            System.out.println(miles);
+        }
+        {
+            int price = 19_999; // 999 миль (19_999 / 20 = 999)
+            int miles = service.calculate(price); // должно получиться 500
+            System.out.println(miles);
+        }
+        {
+            int price = 21; // 1 миля
+            int miles = service.calculate(price); // должно получиться 500
+            System.out.println(miles);
+        }
+        {
+            int price = 19; // 0 миль
+            int miles = service.calculate(price); // должно получиться 500
+            System.out.println(miles);
+        }
+    }
+}
